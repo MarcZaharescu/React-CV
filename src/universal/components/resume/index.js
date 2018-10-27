24a9dd2700d93d918ff54f8aaf093e59d64c943d
MarcZaharescu <zaharescumarc10@gmail.com>
2018-10-27 13:24:09 +0100

    Refractored the resume section

--- src/universal/components/resume/index.js
+++ src/universal/components/resume/index.js
@@ -1,124 +1,146 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Resume = props => { 
+const Resume = props => {
+  const resumeSections = ["EDUCATION", "WORK", "SKILLS"];
+  const educationElements = [
+    {
+      school: "University of Birmingham",
+      degree: "Artificial Intelligence with Computer-Science Bsc",
+      year: "2013 - 2017",
+      description: `Describe what I did here for about three lines.Describe what I
+    did here for about three lines.Describe what I did here for
+    about 2-3 lines`
+    },
+    {
+      school: "Gheorghe Lazar National College",
+      degree: "Maths with Computer-Science",
+      year: "2009 - 2013",
+      description: `Describe what I did here for about three lines.Describe what I
+  did here for about three lines.Describe what I did here for
+  about 2-3 lines`
+    }
+  ];
+
+  const workElements = [
+    {
+      company: "Tesco Plc",
+      title: "Software Engineer",
+      year: "2017 - present",
+      description: `Describe what I did here for about three lines.Describe what I
+    did here for about three lines.Describe what I did here for
+    about 2-3 lines`
+    },
+    {
+      company: "Talmix Plc",
+      title: "Data Analyst",
+      year: "2016 - 2017",
+      description: `Describe what I did here for about three lines.Describe what I
+  did here for about three lines.Describe what I did here for
+  about 2-3 lines`
+    }
+  ];
+
+  const skillsElements = [
+    {
+      name: "JavaScript",
+      level: "60%"
+    },
+    {
+      name: "React",
+      level: "30%"
+    },
+    {
+      name: "NodeJs",
+      level: "40%"
+    },
+    {
+      name: "Redux",
+      level: "30%"
+    },
+
+    {
+      name: "Java",
+      level: "50%"
+    },
+    {
+      name: "AWS",
+      level: "30%"
+    },
+    {
+      name: "Git",
+      level: "60%"
+    }
+  ];
+
+   
+
+
   return (
     <section id="resume">
       <div className="section-row">
-        <div className="resume-row">
-          <div className="left-column">
-            <h3>
-              <span>EDUCATION</span>
-            </h3>
-          </div>
-          <div className="right-column">
-            <div>
-              <h2> University of Birmingham </h2>
-              <p className="resume-items">
-                <span>Artificial Intelligence with Computer-Science Bsc</span>
-                <span> • </span>
-                <span>2013 - 2017</span>
-              </p>
-              <span>
-                Describe what I did here for about three lines.Describe what I
-                did here for about three lines.Describe what I did here for
-                about 2-3 lines.{" "}
-              </span>
+        {resumeSections.map(section => {
+          return (
+            <div className="resume-row">
+              <div className="left-column">
+                <h3>
+                  <span>{section}</span>
+                </h3>
+              </div>
+              {section == "EDUCATION"
+                ? educationElements.map(education => {
+                    return (
+                      <div className="right-column">
+                        <div>
+                          <h2>{education.school}</h2>
+                          <p className="resume-items">
+                            <span>{education.degree}</span>
+                            <span> • </span>
+                            <span>{education.year}</span>
+                          </p>
+                          <span>{education.description}</span>
+                        </div>
+                      </div>
+                    );
+                  })
+                : null}
+
+              {section == "WORK"
+                ? workElements.map(work => {
+                    return (
+                      <div className="right-column">
+                        <div>
+                          <h2>{work.company}</h2>
+                          <p className="resume-items">
+                            <span>{work.title}</span>
+                            <span> • </span>
+                            <span>{work.year}</span>
+                          </p>
+                          <span>{work.description}</span>
+                        </div>
+                      </div>
+                    );
+                  })
+                : null}
+
+                {section == "SKILLS"
+                  ? skillsElements.map(skill => {
+                      return (
+                        <div className="right-column">
+                          <ul className="skills">
+                            <li>
+                              <h2> {skill.name}</h2>
+                              <span> {skill.level} </span>
+                            </li>
+                            </ul>
+                        </div>
+                      );
+                    })
+                  : null}
             </div>
-
-            <div>
-              <h2> Gheorghe Lazar National College </h2>
-              <p className="resume-items">
-                <span>Maths with Computer-Science Bsc</span>
-                <span> • </span>
-                <span>2009 - 2013</span>
-              </p>
-              <span>
-                Describe what I did here for about three lines. Describe what I
-                did here for about three lines. Describe what I did here for
-                about 2-3 lines.{" "}
-              </span>
-            </div>
-                <div className="resume-separator"> <hr /></div>
-          </div>
-
-
+          );
+        })}
         </div>
-
-        <div className="resume-row">
-          <div className="left-column">
-            <h3>
-              <span>WORK</span>
-            </h3>
-          </div>
-          <div className="right-column">
-            <div>
-              <h2> Tesco Plc </h2>
-              <p className="resume-items">
-                <span>Software Engineer</span>
-                <span> • </span>
-                <span>2017 - present</span>
-              </p>
-              <span>
-                Describe what I did here for about three lines.Describe what I
-                did here for about three lines.Describe what I did here for
-                about 2-3 lines.{" "}
-              </span>
-            </div>
-
-            <div>
-              <h2> Talmix PLC </h2>
-              <p className="resume-items">
-                <span>Data Analyst</span>
-                <span> • </span>
-                <span>2016 - 2017</span>
-              </p>
-              <span>
-                Describe what I did here for about three lines. Describe what I
-                did here for about three lines. Describe what I did here for
-                about 2-3 lines.{" "}
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="resume-separator"> <hr /></div>
-        <div className="resume-row">
-          <div className="left-column">
-            <h3>
-              <span>SKILLS</span>
-            </h3>
-          </div>
-          <div className="right-column">
-            <ul className="skills">
-              <li>
-                <h2> JavaScript</h2>
-                <span> 10% </span>
-              </li>
-              <li>
-                <h2> NodeJS</h2>
-                <span> 60% </span>
-              </li>
-              <li>
-                <h2> React</h2>
-                <span> 70% </span>
-              </li>
-              <li>
-                <h2> Redux</h2>
-                <span> 50% </span>
-              </li>
-              <li>
-                <h2> Java</h2>
-                <span> 30% </span>
-              </li>
-              <li>
-                <h2> Git</h2>
-                <span> 20% </span>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="resume-separator"> <hr /></div>
-      </div>
     </section>
   );
 };
